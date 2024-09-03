72aa1df4daca1e930c3e81f1403006100dd47179
DimitraK92 <degraded_rebuilt@tutanota.com>
2024-09-03 19:20:55 +0300

    enhanced the info displayed to the user for their guess based on range

--- helper.py
+++ helper.py
@@ -12,12 +12,30 @@ def validate_user_input_level(user_input):
 
 def validate_user_input_limit(user_input, limit):
     user_input = validate_user_input_number(user_input)
-    while user_input not in range(1, limit):
-        user_input = input(f"Only numbers between 1 and {limit} are allowed. Try again.\n")
+    while user_input not in range(1, limit+1):
+        user_input = input(f"Only numbers between 1 and {limit} are allowed. Try again: ")
     return user_input
 
 
 def validate_user_input_play_again(user_input):
     while user_input.lower() not in ["y", "n"]:
-        user_input = input("Wrong input. Try again.\n").lower()
+        user_input = input("Wrong input. Try again: ").lower()
     return user_input
+
+
+def handle_low_guess(user_guess, number_to_guess, guess_range):
+    if user_guess < number_to_guess - (guess_range * 3):
+        print("Extremely low guess.")
+    elif user_guess < number_to_guess - (guess_range * 2):
+        print("Too low guess.")
+    else:
+        print("Low guess.")
+
+
+def handle_high_guess(user_guess, number_to_guess, guess_range):
+    if user_guess > number_to_guess + (guess_range * 3):
+        print("Extremely high guess.")
+    elif user_guess > number_to_guess + (guess_range * 2):
+        print("Too high guess.")
+    else:
+        print("High guess.")
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 ﻿from art import logo
-from helper import validate_user_input_level, validate_user_input_number, validate_user_input_limit, \
-    validate_user_input_play_again
+import helper
 import random
 import signal
 from os import system
@@ -25,33 +24,31 @@ def start_game():
 def play_game():
     system("cls")
     print(logo)
-    limit = validate_user_input_number(input("Until which number would you like to guess: "))
+    limit = helper.validate_user_input_number(input("Until which number would you like to guess: "))
     number_to_guess = random.randint(1, limit)
-    difficulty = validate_user_input_level(input("Choose a difficulty. Type 'easy' or 'hard': "))
+    difficulty = helper.validate_user_input_level(input("Choose a difficulty. Type 'easy' or 'hard': "))
     attempts = 10
     if difficulty != "easy": attempts = 5
     print(f"You have {attempts} attempts remaining to guess the number.")
-    user_guess = validate_user_input_limit(input("Make a guess: "), limit)
+    user_guess = helper.validate_user_input_limit(input("Make a guess: "), limit)
+    guess_range = limit / 10
     win = False
     for i in reversed(range(1, attempts)):
-        win = (check_user_guess(user_guess, number_to_guess))
+        win = check_user_guess(user_guess, number_to_guess, guess_range)
         if win:
             print(f"You got it! The answer is {user_guess}")
             break
         if not win:
             print(f"You have {i} attempts remaining to guess the number.")
-            user_guess = validate_user_input_limit(input("Guess again: "), limit)
+            user_guess = helper.validate_user_input_limit(input("Guess again: "), limit)
     if not win: print("You run out of guesses, you lose")
-    return validate_user_input_play_again(input("Do you want to play again? (y/n) ")) == 'y'
+    return helper.validate_user_input_play_again(input("Do you want to play again? (y/n) ")) == 'y'
 
 
-def check_user_guess(user_guess, number_to_guess):
-    if user_guess == number_to_guess:
-        return True
-    if user_guess < number_to_guess:
-        print("Too low.")
-    else:
-        print("Too high.")
+def check_user_guess(user_guess, number_to_guess, guess_range):
+    if user_guess == number_to_guess: return True
+    if user_guess < number_to_guess: helper.handle_low_guess(user_guess, number_to_guess, guess_range)
+    else: helper.handle_high_guess(user_guess, number_to_guess, guess_range)
     return False
 
 
